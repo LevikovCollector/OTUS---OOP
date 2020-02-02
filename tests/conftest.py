cdabc5619d0eb9cc8b2a21c90376225d9729671c
Vladimir <v.v.levikov@gmail.com>
2020-02-02 14:38:12 +0300

    Домашнее задание по работе с классами

---
+++ tests/conftest.py
@@ -0,0 +1,20 @@
+import pytest
+from figure_objects import Triangle, Rectangle, Circle, SquareFigure
+
+@pytest.fixture
+def create_triangle():
+    return Triangle('Новый треугольник', 5, 6, 7)
+
+@pytest.fixture
+def create_rectangle():
+    return Rectangle('Новый прямоугольник', 3, 5)
+
+@pytest.fixture
+def create_circle():
+    return Circle('Новый круг', 8)
+
+@pytest.fixture
+def create_square():
+    return SquareFigure('Новый квадрат', 7)
+
+
